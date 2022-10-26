887f3121ac893dba7d1c68863e7d40449706307f
rezoneprince <rezoneprinceme@gmail.com>
2022-10-26 18:01:46 +0600

    data added and api ready

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const category = require("./data/category.json");
+const courses = require("./data/courses.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Learn Hub API");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/categories", (req, res) => {
+  res.send(category);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.filter((c) => c.category_id === id);
+  res.send(course);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((c) => c._id === id);
+  res.send(course);
+});
 
 app.listen(port, () => {
-    console.log(`server is running http://localhost:${port}`)
-})
+  console.log(`server is running http://localhost:${port}`);
+});
